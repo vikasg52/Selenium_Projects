9b13143565be55d3ed32d1c0aae5f586977ecf27
santoshsharma27 <santosh37kr@gmail.com>
2022-05-31 23:21:59 +0530

    code on 31st May

---
+++ OOPConceptInPOM/src/main/java/com/mypages/HomePage.java
@@ -0,0 +1,12 @@
+package com.mypages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage{
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
