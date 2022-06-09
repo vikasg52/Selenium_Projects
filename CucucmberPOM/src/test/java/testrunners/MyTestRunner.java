035de2b61119a3a8ced5e0d89d1864fe857e8701
santoshsharma27 <santosh37kr@gmail.com>
2022-06-09 11:55:20 +0530

    code on 9th June

--- CucucmberPOM/src/test/java/testrunners/MyTestRunner.java
+++ CucucmberPOM/src/test/java/testrunners/MyTestRunner.java
@@ -6,8 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\santo\\eclipse-workspace\\Selenium_Projects\\CucucmberPOM\\src\\test\\java\\com\\qa\\features\\freecrm.feature", 
-glue = {"com\\qa\\stepDefinitions" }, // the path of the step definition files
+@CucumberOptions(
+		features = {"src/test/resources/AppFeatures"}, 
+glue = {"stepdefinitions", "AppHooks" }, // the path of the step definition files
 		monochrome = true, // display the console output in a proper readable format
 		// strict = true, //it will check if any step is not defined in step definition
 		// file
